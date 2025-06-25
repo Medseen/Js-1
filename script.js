62347bfe42613c27c82a5421b967574577015e3f
Medseen <johntakeouehara@gmail.com>
2025-06-25 20:30:59 -0300

    kj kj

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ alert ("Comando para mostrar um texto na tela , posso usar aspas duplas ou aspas
 alert ("Ele é limitado ao clique no ok pelo usuário")
 
 alert ("Não é possível configurar seu tempo de permanência na tela")
+
+let numero = 25;
+let chute;
+let idade;
+let nome;
+
+console.log (numero);
