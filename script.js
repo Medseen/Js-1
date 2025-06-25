70823c47fea8db48631e009a3a41893840f3d672
Medseen <johntakeouehara@gmail.com>
2025-06-25 20:22:02 -0300

    kmolikm

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+alert ('Seja bem-vindo ao nosso jogo!');
+alert ("Comando para mostrar um texto na tela , posso usar aspas duplas ou aspas simples, mas siga este padrão em todo código")
 
+alert ("Ele é limitado ao clique no ok pelo usuário")
+
+alert ("Não é possível configurar seu tempo de permanência na tela")
