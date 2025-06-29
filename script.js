f8f30a630569c3975db951b01a88538e45d5c9d7
Medseen <johntakeouehara@gmail.com>
2025-06-28 22:11:37 -0300

    vgjvgj

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-alert ('Seja bem-vindo ao nosso jogo!');
-alert ("Comando para mostrar um texto na tela , posso usar aspas duplas ou aspas simples, mas siga este padrão em todo código")
+alert('Seja bem-vindo ao nosso jogo!');
 
-alert ("Ele é limitado ao clique no ok pelo usuário")
+// Gera um número secreto aleatório entre 1 e 10
+let numeroSecreto = 5;
+console.log(numeroSecreto); // (Para testes, pode remover depois)
 
-alert ("Não é possível configurar seu tempo de permanência na tela")
-
-let numero = 25;
+// Variável para armazenar o chute do usuário
 let chute;
-let idade;
-let nome;
+let tentativas = 0; // Inicia o contador de tentativas
 
-console.log (numero);
+while (chute != numeroSecreto) {
+  chute = parseInt(prompt('Escolha um número entre 1 e 10'));
+  tentativas++; // Soma +1 a cada tentativa
+
+  if (chute == numeroSecreto) {
+    alert(`Parabéns! Você acertou o número secreto ${numeroSecreto} na tentativa ${tentativas}.`);
+  } else if (chute > numeroSecreto) {
+    alert(`O número secreto é menor que ${chute}. Tente novamente.`);
+  } else {
+    alert(`O número secreto é maior que ${chute}. Tente novamente.`);
+  }
+}
