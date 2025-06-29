65d73f0ec39246eca17ecfb0c004dfdf2d78cde0
Medseen <johntakeouehara@gmail.com>
2025-06-28 22:15:34 -0300

    hhcftht

--- script.js
+++ script.js
@@ -1,22 +1,14 @@
-alert('Seja bem-vindo ao nosso jogo!');
+function jogar() {
+  alert("Seja bem-vindo ao Jogo da Média!");
+  
+  let quantidade = parseInt(prompt("Quantos números você quer digitar?"));
+  let soma = 0;
 
-// Gera um número secreto aleatório entre 1 e 10
-let numeroSecreto = 5;
-console.log(numeroSecreto); // (Para testes, pode remover depois)
-
-// Variável para armazenar o chute do usuário
-let chute;
-let tentativas = 0; // Inicia o contador de tentativas
-
-while (chute != numeroSecreto) {
-  chute = parseInt(prompt('Escolha um número entre 1 e 10'));
-  tentativas++; // Soma +1 a cada tentativa
-
-  if (chute == numeroSecreto) {
-    alert(`Parabéns! Você acertou o número secreto ${numeroSecreto} na tentativa ${tentativas}.`);
-  } else if (chute > numeroSecreto) {
-    alert(`O número secreto é menor que ${chute}. Tente novamente.`);
-  } else {
-    alert(`O número secreto é maior que ${chute}. Tente novamente.`);
+  for (let i = 1; i <= quantidade; i++) {
+    let numero = parseFloat(prompt(`Digite o número ${i}:`));
+    soma += numero;
   }
+
+  let media = soma / quantidade;
+  document.getElementById("resultado").innerText = `A média dos ${quantidade} números é: ${media.toFixed(2)}`;
 }
